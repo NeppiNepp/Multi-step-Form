eb04ed61663d7a85ef8c7aefcfecc9f6224362ee
NeppiNepp <gagescarberry@aol.com>
2025-06-20 11:13:15 -0400

    Updated JS
    Modified button functionality & added validation to inputs.

--- script.js
+++ script.js
@@ -3,6 +3,12 @@ const stepBtns = document.getElementsByClassName("step-number");
 const prevBtn = document.getElementById("go-back-btn");
 const nextBtn = document.getElementById("next-step-btn");
 const confirmBtn = document.getElementById("confirm");
+const personalInfo = document.getElementsByName("personal");
+const planChoice = document.getElementsByName("plan-btn");
+const addonsAdded = document.getElementsByName("addon-btn");
+
+//all choices have 0-2 for array
+
 
 const userData = {
     name: "",
@@ -16,7 +22,7 @@ const userData = {
 }
 
 const validEmail = /^([a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\.[a-zA-z]{2,})$/;
-const validPhoneNumber = /^(1[0-9]{9})$/;
+const validPhoneNumber = /^(1[0-9]{10})$/;
 
 
 let i = 0;
@@ -75,8 +81,43 @@ const hideTab = () => { // toggles page off
 
 const validateSection = () => {
     // working on validing sections based on index number, then showing an invalid response if false
-    let valid = true;
+    const nameInput = personalInfo[0];
+    const emailInput = personalInfo[1];
+    const phoneInput = personalInfo[2];
+    let valid = false;
 
+    if (i === 0) { // validate personal info section
+        if (nameInput.value) {
+            valid = true;
+            nameInput.style.border = "1px solid lightgray";
+        } else {
+            nameInput.style.border = "2px solid red";
+        }
+        if (validEmail.test(emailInput.value)) {
+            valid = true;
+            emailInput.style.border = "1px solid lightgray";
+        } else {
+            emailInput.style.border = "2px solid red";
+        }
+        if (validPhoneNumber.test(phoneInput.value)) {
+            valid = true;
+            phoneInput.style.border = "1px solid lightgray";
+        } else {
+            phoneInput.style.border = "2px solid red";
+        }
+    }
+
+
+    if (i === 1) { // validate that plan has been chosen
+        Array.from(planChoice).forEach(choice => {
+            if (choice.checked) {
+                valid = true;
+            }
+        })
+    }
+
+    if (i > 1) // default for other tabs
+        valid = true;
 
     return valid;
 }
@@ -99,9 +140,8 @@ const validateSection = () => {
 
 Array.from(stepBtns).forEach((btn, index) => { // adds functionality to step buttons
     btn.addEventListener("click", () => {
-        if (i === 4)
+        if (!validateSection() || i === 4 || i < index) // checks if current tab data is valid, form is not on last tab, & step button is before the current step
             return;
-
         hideTab();
         i = index;
         showTab();
@@ -109,6 +149,10 @@ Array.from(stepBtns).forEach((btn, index) => { // adds functionality to step but
 })
 
 nextBtn.addEventListener("click", () => {
+    if (!validateSection()) {
+        console.log(validateSection());
+        return;
+    }
     hideTab();
     i++;
     showTab();
